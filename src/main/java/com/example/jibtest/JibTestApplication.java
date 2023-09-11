a7a388e52bb36378ddb7627e745071ac9a7a8357
hseungho <42243302+hseungho@users.noreply.github.com>
2023-09-11 22:40:52 +0900

    Release 0.0.1

--- src/main/java/com/example/jibtest/JibTestApplication.java
+++ src/main/java/com/example/jibtest/JibTestApplication.java
@@ -2,6 +2,9 @@ package com.example.jibtest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @SpringBootApplication
 public class JibTestApplication {
@@ -10,4 +13,9 @@ public class JibTestApplication {
         SpringApplication.run(JibTestApplication.class, args);
     }
 
+    @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
+    public String hello() {
+        return "Hello JIB World";
+    }
 }
