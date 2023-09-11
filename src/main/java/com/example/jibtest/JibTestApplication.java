7a15db84c299ca67ace5f6eb7cb095f4639b2ebe
hseungho <hsh47607@gmail.com>
2023-09-11 21:05:33 +0900

    first commit

---
+++ src/main/java/com/example/jibtest/JibTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jibtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JibTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JibTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jibtest/JibTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jibtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JibTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
