3bba3f59284c2f2b5df36a4a7e26021d51587e6e
hseungho <hsh47607@gmail.com>
2023-09-18 22:58:46 +0900

    release 0.0.23

--- src/main/java/com/example/jibtest/JibTestApplication.java
+++ src/main/java/com/example/jibtest/JibTestApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JibTestApplication {
 
     public static void main(String[] args) {
